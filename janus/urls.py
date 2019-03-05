d6af82b5229f364f816a2f413c44185887a0bdad
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2019-03-05 02:19:56 -0600

    Added lots favicon, took a while

--- janus/urls.py
+++ janus/urls.py
@@ -19,6 +19,8 @@ from django.urls import include
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.storage import staticfiles_storage
+
 
 
 urlpatterns = [
