e213453a2cd6c8cf76a639c3f5216880bf88966a
Henry O'Callaghan <henry.ocallaghan@gmail.com>
2019-03-08 04:38:18 -0600

    Got GET working with ESP. CORS issue, still need to fix on ESP. Bypassed temporarily with browser extension

--- static/js/choose.js
+++ static/js/choose.js
@@ -1,11 +1,12 @@
+
 var HttpClient = function() {
     this.get = function(aUrl, aCallback) {
         var anHttpRequest = new XMLHttpRequest();
         anHttpRequest.onreadystatechange = function() {
             if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
-                aCallback(anHttpRequest.response);
+                aCallback(anHttpRequest.responseText);
         }
-        anHttpRequest.open("GET", aUrl, true);
+        anHttpRequest.open("GET", aUrl, false);
         anHttpRequest.send();
     }
 }
@@ -17,7 +18,50 @@ function recieveHeartbeat(response) {
 function recieveValue(response) {
   window.alert("Randomnumber: " + response);
 }
-
 var client = new HttpClient();
-//client.get('http://100.64.13.129/', recieveHeartbeat);
-client.get('https://www.random.org/integers/?num=1&min=1&max=5&col=1&base=10&format=plain&rnd=new', recieveValue);
+client.get('http://100.64.13.129/heartbeat', recieveHeartbeat);
+client.get('http://100.64.13.129/numberRequest', recieveValue);
+
+
+/*
+function makeHttpObject() {
+  try {return new XMLHttpRequest();}
+  catch (error) {}
+  try {return new ActiveXObject("Msxml2.XMLHTTP");}
+  catch (error) {}
+  try {return new ActiveXObject("Microsoft.XMLHTTP");}
+  catch (error) {}
+
+  throw new Error("Could not create HTTP request object.");
+}
+
+var request = makeHttpObject();
+request.open("GET", "http://100.64.13.129/heartbeat", false);
+request.send();
+window.alert(request.getAllResponseHeaders());
+*/
+//
+/*
+$.ajax({
+  url: "http://100.64.13.129/heartbeat",
+  error: AjaxFailed,
+  success: function(data) {
+    window.alert(data);
+    //do something when request is successfull
+  },
+  dataType: "text"
+});
+
+//
+
+$.get(
+  "http://100.64.13.129/heartbeat",
+  function(data, status){
+    alert("Data: " + data + "\nStatus: " + status);
+  });
+
+function AjaxFailed(result) {
+    //alert("hello1");
+    alert(result.status + ' ' + result.statusText);
+}
+*/
